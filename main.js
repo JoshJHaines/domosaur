aba9f9ea4a82b6fcb702651f9db62b958139e472
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-16 12:18:04 -0400

    idk

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const heading = document.querySelector("h1");
+const messWithMeMore = document.querySelector(".mess-with-me-more");
 const messWithMe = document.querySelector(".mess-with-me");
+const hideMe = document.querySelector("#hide-me");
+const triTop = document.querySelector("#triceratops");
+const button = document.querySelector("#toggle")
+const spanny = document.querySelector("span")
 
 function makeBold() {
 	heading.style.fontWeight = 600;
@@ -9,3 +14,19 @@ function makeBold() {
 
 messWithMe.style.fontSize = "40px";
 messWithMe.style.backgroundColor = "green";
+
+hideMe.style.display = "none";
+triTop.style.width = "324px";
+
+function makeOrange() {
+    const orangeText = document.querySelector(".mess-with-me");
+    orangeText.style.color = "orange";
+}
+
+messWithMe.addEventListener("onclick", makeOrange);
+
+// function fontColor(){
+//     const orangeFont =document.querySelector('.mess-with-me');
+//     orangeFont.style.color = "orange";
+// }
+// msmbg.addEventListener('click',fontColor);
