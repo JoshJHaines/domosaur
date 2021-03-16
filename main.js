a1598f5b4cb7e0385813b41bfccbb26823008ae7
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-16 11:27:32 -0400

    commented out first addEventListener. Created messWithMe and changed font size and backgroubd color

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const heading = document.querySelector("h1");
+const messWithMe = document.querySelector(".mess-with-me");
 
 function makeBold() {
 	heading.style.fontWeight = 600;
 }
 
-heading.addEventListener("click", makeBold);
+// heading.addEventListener("click", makeBold);
+
+messWithMe.style.fontSize = "40px";
+messWithMe.style.backgroundColor = "green";
