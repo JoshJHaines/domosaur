0f743f0440fefb5d224968ccd7f5107b04c9d0f0
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-16 12:33:48 -0400

    finished mandatory

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const heading = document.querySelector("h1");
 const messWithMe = document.querySelector(".mess-with-me");
 const hideMe = document.querySelector("#hide-me");
 const triTop = document.querySelector("#triceratops");
+const feather = document.querySelector("#feathers");
+const dinos = document.querySelector("#row");
+const biggy = document.querySelector("#biggify")
 const button = document.querySelector("#toggle")
 // const spanny = document.querySelector("span")
 
@@ -24,3 +27,33 @@ function makeOrange() {
 }
 
 messWithMe.addEventListener("click", makeOrange);
+
+
+function redBorder(){
+    const addRed = document.querySelector("#triceratops");
+    addRed.style.border = "2px solid red"
+}
+
+triTop.addEventListener("click", redBorder)
+
+function opa(){
+    const addRed = document.querySelector("#feathers");
+    addRed.style.opacity = "0.5"
+}
+
+feather.addEventListener("click", opa)
+
+function dinoBack(){
+    const background = document.querySelector("#row");
+    background.style.backgroundColor = "blue"
+}
+
+button.addEventListener("click", dinoBack)
+
+
+function biggify(){
+    const makeBig = document.querySelector("#biggify");
+    makeBig.style.width = "200px"
+}
+
+biggy.addEventListener("mouseover", biggify)
